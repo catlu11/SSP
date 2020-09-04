22184e59e6f01da31f0082dc94d9a83ff093c132
catlu11 <2111catlu@gmail.com>
2020-09-03 18:50:13 -0700

    Reorganize od5.py

--- od5.py
+++ od5.py
@@ -13,50 +13,18 @@ c = 173.145
 
 
 # AIJ DATA
-RA = [RAdecimalToHMS(17.633096*15), RAdecimalToHMS(17.642617*15),
-      RAdecimalToHMS(17.661737*15)]
-DEC = [DECdecimalToDMS(-24.906182), DECdecimalToDMS(-22.779707),
-       DECdecimalToDMS(-21.790782)]
-t1 = 2458671.778
+t1 = 2458671.778 # in Julian days
 t2 = 2458679.772
 t3 = 2458683.774
+RA = [RAdecimalToHMS(17.633096*15), RAdecimalToHMS(17.642617*15),
+      RAdecimalToHMS(17.661737*15)] # RA coordinates of asteroid at t1, t2, and t3
+DEC = [DECdecimalToDMS(-24.906182), DECdecimalToDMS(-22.779707), 
+       DECdecimalToDMS(-21.790782)] # DEC coordations of asteroid at t1, t2, and t3
 R = [[-2.568180989317604E-01,  9.026400560686667E-01,  3.912553385188193E-01],
        [-3.849247965056879E-01, 8.631971645323986E-01, 3.741557451122443E-01],
-      [-4.465913217555757E-01, 8.375970194890799E-01,  3.630559556728792E-01]] #in equatorial coordinates REAL
+      [-4.465913217555757E-01, 8.375970194890799E-01,  3.630559556728792E-01]] # in equatorial coordinates 
 expected_elements = [2.640226301080940 , 4.059008132819690E-01, 9.527069429767748,
                      2.809685769461632E+02 , 3.567195418376240E+02, 1.777935844299149] # EXPECTED VALUES OF a, e, i, big om, w, M
-
-
-# Convert RA and DEC coordinates to radians
-RA_rad = [HMStoDeg(x[0], x[1], x[2]) * pi/180 for x in RA]
-DEC_rad = [DMStoDeg(x[0], x[1], x[2]) * pi/180 for x in DEC]
-
-
-# Calculate rho hat from RA and DEC values
-rho_x = [cos(RA_rad[x])*cos(DEC_rad[x]) for x in range(len(RA_rad))]
-rho_y = [sin(RA_rad[x])*cos(DEC_rad[x]) for x in range(len(RA_rad))]
-rho_z = [sin(DEC_rad[x]) for x in range(len(RA_rad))]
-rho_hat_calc = [[rho_x[x], rho_y[x], rho_z[x]] for x in range(len(RA))]
-rho_hat = np.array(rho_hat_calc)
-
-
-# Adjust for time difference w/ taus
-tau1 = k*(t1-t2)
-tau3 = k*(t3-t2)
-tau0 = tau3 - tau1
-
-
-# Approximate a1 and a3
-a1 = tau3/tau0
-a3 = -tau1/tau0
-count = 0
-
-
-# Set starting values for approximation
-r2 = [3]
-r2dot = [3]
-r2o = [0]
-r2doto = [0]
 
 
 # f series function
@@ -68,70 +36,101 @@ def f(tau):
                  15*(dot(r2, r2dot)/(mag(r2)**2))**2 + 1/(mag(r2)**3)
     return series_pt1 + coeff*disgusting
 
+
 # g series function
 def g(tau):
     return tau - (tau**3)/(6*(mag(r2)**3)) + (tau**4)*(dot(r2, r2dot))/(4*mag(r2)**5)
 
 
 # Determine if two vectors are similar enough to be considered equivalent
-def determineClose(v1, v2):
+def determine_close(v1, v2):
     for i in range(len(v1)):
         if(abs(v1[i]-v2[i]) > 10**-11):
             return False
     return True
 
 
-# Method of Gauss algorithm to approximate r2 and r2dot
-while(determineClose(r2, r2o) == False and determineClose(r2dot, r2doto) == False and count < 10000):
-    D0 = dot(rho_hat[0], cross(rho_hat[1], rho_hat[2]))
-    D = np.ones((3,3))
-    for j in range(3):
-        D[0,j] = dot(cross(R[j], rho_hat[1]), rho_hat[2])
-        D[1,j] = dot(cross(rho_hat[0], R[j]), rho_hat[2])
-        D[2,j] = dot(rho_hat[0], cross(rho_hat[1], R[j]))
-    
-    # Calculate rhos
-    rho = np.ones(3)
-    rho[0] = (a1*D[0,0]+(-1)*D[0,1]+a3*D[0,2])/(a1*D0)
-    rho[1] = (a1*D[1,0]+(-1)*D[1,1]+a3*D[1,2])/(-1*D0)
-    rho[2] = (a1*D[2,0]+(-1)*D[2,1]+a3*D[2,2])/(a3*D0)
+def calc_r2(t1, t2, t3, R, RA, DEC):
+      # Convert RA and DEC coordinates to radians
+      RA_rad = [HMStoDeg(x[0], x[1], x[2]) * pi/180 for x in RA]
+      DEC_rad = [DMStoDeg(x[0], x[1], x[2]) * pi/180 for x in DEC]
 
-    # Adjust taus
-    tau1 = k*((t1-rho[0]/c)-(t2-rho[1]/c))
-    tau3 = k*((t3-rho[2]/c)-(t2-rho[1]/c))
-    tau0 = tau3 - tau1
-    
-    # Approximate vectors r1, r2, r3, r2dot
-    rvec = []
-    for i in range(3):
-        rvec.append(rho[i]*rho_hat[i] - R[i])
-    rvec = np.array(rvec)
-    r2o = r2
-    r2 = rvec[1]
-    if(count == 0):
-        r2dot = (rvec[2]-rvec[0])/tau0
+      # Calculate rho hat from RA and DEC values
+      rho_x = [cos(RA_rad[x])*cos(DEC_rad[x]) for x in range(len(RA_rad))]
+      rho_y = [sin(RA_rad[x])*cos(DEC_rad[x]) for x in range(len(RA_rad))]
+      rho_z = [sin(DEC_rad[x]) for x in range(len(RA_rad))]
+      rho_hat_calc = [[rho_x[x], rho_y[x], rho_z[x]] for x in range(len(RA))]
+      rho_hat = np.array(rho_hat_calc)
 
-    # Calculate f and g series
-    f1 = f(tau1)
-    f3 = f(tau3)
-    g1 = g(tau1)
-    g3 = g(tau3)
+      # Adjust for relativistic time difference w/ taus
+      tau1 = k*(t1-t2)
+      tau3 = k*(t3-t2)
+      tau0 = tau3 - tau1
 
-    # Update a and b
-    a1 = g3/(f1*g3-f3*g1)
-    a3 = g1/(f3*g1-f1*g3)
-    b1 = f3/(f3*g1-f1*g3)
-    b3 = f1/(f1*g3-f3*g1)
+      # Approximate a1 and a3
+      a1 = tau3/tau0
+      a3 = -tau1/tau0
+      count = 0
 
-    # Update r2dot
-    r2doto = r2dot
-    r2dot = b1*rvec[0] + b3*rvec[2]
-    count += 1
+      # Set starting values for approximation
+      r2 = [3]
+      r2dot = [3]
+      r2o = [0]
+      r2doto = [0]
+      
+      # Method of Gauss algorithm to approximate r2 and r2dot
+      while(determine_close(r2, r2o) == False and determine_close(r2dot, r2doto) == False and count < 10000):
+          D0 = dot(rho_hat[0], cross(rho_hat[1], rho_hat[2]))
+          D = np.ones((3,3))
+          for j in range(3):
+              D[0,j] = dot(cross(R[j], rho_hat[1]), rho_hat[2])
+              D[1,j] = dot(cross(rho_hat[0], R[j]), rho_hat[2])
+              D[2,j] = dot(rho_hat[0], cross(rho_hat[1], R[j]))
+
+          # Calculate rhos
+          rho = np.ones(3)
+          rho[0] = (a1*D[0,0]+(-1)*D[0,1]+a3*D[0,2])/(a1*D0)
+          rho[1] = (a1*D[1,0]+(-1)*D[1,1]+a3*D[1,2])/(-1*D0)
+          rho[2] = (a1*D[2,0]+(-1)*D[2,1]+a3*D[2,2])/(a3*D0)
+
+          # Adjust taus
+          tau1 = k*((t1-rho[0]/c)-(t2-rho[1]/c))
+          tau3 = k*((t3-rho[2]/c)-(t2-rho[1]/c))
+          tau0 = tau3 - tau1
+
+          # Approximate vectors r1, r2, r3, r2dot
+          rvec = []
+          for i in range(3):
+              rvec.append(rho[i]*rho_hat[i] - R[i])
+          rvec = np.array(rvec)
+          r2o = r2
+          r2 = rvec[1]
+          if(count == 0):
+              r2dot = (rvec[2]-rvec[0])/tau0
+
+          # Calculate f and g series
+          f1 = f(tau1)
+          f3 = f(tau3)
+          g1 = g(tau1)
+          g3 = g(tau3)
+
+          # Update a and b
+          a1 = g3/(f1*g3-f3*g1)
+          a3 = g1/(f3*g1-f1*g3)
+          b1 = f3/(f3*g1-f1*g3)
+          b3 = f1/(f1*g3-f3*g1)
+
+          # Update r2dot
+          r2doto = r2dot
+          r2dot = b1*rvec[0] + b3*rvec[2]
+          count += 1
 
       
-# Convert r2 vectors to the ecliptic plane
-r2 = rotate_x(r2, 23.4367505323)
-r2dot = rotate_x(r2dot, 23.4367505323)
+      # Convert r2 vectors to the ecliptic plane
+      r2 = rotate_x(r2, 23.4367505323)
+      r2dot = rotate_x(r2dot, 23.4367505323)
+      
+      return (r2, r2dot)
 
 
 # Calculate orbital elements based on r2
@@ -200,10 +199,8 @@ def format(expected, actual, errors):
           "Percent error:", errors[5]*100,"%")
 
 
-def main(r, rdot, expected):
-    elements = calc_orbital_elements(r, rdot)
-    errors = calc_percent_error(expected, elements)
-    format(expected, elements, errors)
-
-
-main(r2, r2dot, expected_elements)
+if __name__ == '__main__':
+    r2s = calc_r2(t1, t2, t3, R, RA, DEC)
+    calc_elements = calc_orbital_elements(r2s[0], r2s[1])
+    errors = calc_percent_error(expected_elements, calc_elements)
+    format(expected_elements, calc_elements, errors)
